25f7df374f436099eec440c42d221bd83b322bdc
Shubhranshu Sekhar Sethi <131181092+Shubhranshu6370@users.noreply.github.com>
2024-02-25 23:46:40 +0530

    Add files via upload

---
+++ max-num.js
@@ -0,0 +1,14 @@
+//Find maximum number from array
+
+let arr = [14, 32, 39, 50, 60, 78];
+
+function findNum() {
+    let max = arr.reduce((max, el) => {
+        if (max > el) {
+            return max;
+        } else {
+            return el;
+        }
+    });
+    return max;
+}
